69913352cf5b5fc5c2a6876e9159bfbc91ee08ce
Rock1ey <2061218098@qq.com>
2026-04-20 14:01:07 +0800

    取消torch compile

--- train_cmd.sh
+++ train_cmd.sh
@@ -20,3 +20,6 @@ python -m tools.sample_ddpm_cond_hemit --config config/hemit_phikon_sc.yaml --fu
 
 # 第四步：评估
 python -m tools.evaluate_hemit --config config/hemit_phikon_sc.yaml
+
+# phikon_img_cond
+torchrun --nproc_per_node=2 -m tools.train_ddpm_cond_hemit --config config/hemit_phikon_img.yaml
